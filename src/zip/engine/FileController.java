6764d76844b3bb8df5a0f02c880a84445c9d73f9
Luciano Moura <lucke1979@gmail.com>
2021-04-26 05:42:30 -0300

    Vê se não perde denovo

--- src/zip/engine/FileController.java
+++ src/zip/engine/FileController.java
@@ -13,7 +13,7 @@ public class FileController {
 	public static String word = "";
 	public static LinkedList list = new LinkedList();
 	public static String text = "";
-	
+
 	public static void main(String[] args) throws IOException {
 		readFile();
 		zipFile();
@@ -21,47 +21,50 @@ public class FileController {
 		System.out.println(list.length());
 		System.out.println();
 		System.out.println(text);
-		
+
 	}
-	
+
 	public static void zipFile() throws IOException {
 		int c = 0;
 		while ((c = br.read()) != -1) {
 			char character = (char) c;
 			String st = Character.toString(character);
-			if(st.matches("([a-zA-Z\n\s,-.']+)")) {
-				if(st.matches("[a-zA-Z]+"))
+			if (st.matches("([a-zA-Z\n\s,-.']+)")) {
+				if (st.matches("[a-zA-Z]+"))
 					word += st;
-				if(st.matches("([\n\s,-.']+)")) {
-					word = addWordAtList(word.replace("-", "").replace(",", "").replace(".", "").replace(" ", "").trim());
+				if (st.matches("([\n\s,-.']+)")) {
+					if (word.length() > 0) {
+						word = addWordAtList(
+								word.replace("-", "").replace(",", "").replace(".", "").replace(" ", "").trim());
+						System.out.println("Words to add: " + word);
 						text += word;
-//						System.out.println("New Word: " + word);
 						word = "";
-//						System.out.println(word);
-////						System.out.println(text);
+					}
 				}
-				if(st.matches("([^a-zA-Z])")){
+				if (st.matches("([^a-zA-Z])")) {
 					text += st;
 				}
 			}
-			
-//			System.out.println(st);
+
 		}
 	}
-	
+
 	public static void readFile() throws FileNotFoundException {
 		file = new File("filename.txt");
 		fr = new FileReader(file);
 		br = new BufferedReader(fr);
 	}
-	
+
 	public static String addWordAtList(String word) {
-		if(!word.isBlank())
-			System.out.println("Words to add: " + word);
-			if(!list.findWord(word)) {
+		if (!word.isEmpty())
+			if (!list.findWord(word)) {
 				list.insertWord(word);
 				return word;
 			}
-			return Integer.toString(list.findIndexByWord(word));
+		return Integer.toString(list.findIndexByWord(word));
+	}
+	
+	public void saveFile() {
+		
 	}
 }
